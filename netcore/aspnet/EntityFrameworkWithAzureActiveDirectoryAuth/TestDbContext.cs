656245d49e7cde45fb69a653507631d46207cac4
Daniel Espinosa <dan.development@live.com>
2020-08-30 14:53:10 -0700

    added documentation.

--- netcore/aspnet/EntityFrameworkWithAzureActiveDirectoryAuth/TestDbContext.cs
+++ netcore/aspnet/EntityFrameworkWithAzureActiveDirectoryAuth/TestDbContext.cs
@@ -6,6 +6,8 @@ namespace EntityFrameworkWithAzureActiveDirectoryAuth
     {
         public TestDbContext(DbContextOptions options) : base(options)
         {
+            // This is commented since the LocalDb does not take AAD tokens.
+            // In the real life scenario, where the DB was an Azure SQL server this code would get the AAD token and set it in the connection each HTTP request.
             //if (Database.IsSqlServer())
             //{
             //    SqlConnection connection = (SqlConnection)Database.GetDbConnection();
