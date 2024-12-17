fba4a5312788b7e3e10a8333ba17ee455c1d426b
Dan Espinosa <30415120+danespinosa@users.noreply.github.com>
2024-12-16 23:11:14 -0800

    Create Install-DotnetDump.ps1 (#3)
    added install-dotnetDump that downloads dotnet and dotnet-dump to easily debug containers

---
+++ scripts/Install-DotnetDump.ps1
@@ -0,0 +1,45 @@
+$ErrorActionPreference="Stop"
+$ProgressPreference="SilentlyContinue"
+
+# $LocalDotnet is the path to the locally-installed SDK to ensure the
+#   correct version of the tools are executed.
+$LocalDotnet=""
+# $InstallDir and $CliVersion variables can come from options to the
+#   script.
+$InstallDir = "c:\dotnet"
+$CliVersion = "6.0.428"
+
+# Test the path provided by $InstallDir to confirm it exists. If it
+#   does, it's removed. This is not strictly required, but it's a
+#   good way to reset the environment.
+if (Test-Path $InstallDir)
+{
+    rm -Recurse $InstallDir
+}
+New-Item -Type "directory" -Path $InstallDir
+
+Write-Host "Downloading the CLI installer..."
+
+# Use the Invoke-WebRequest PowerShell cmdlet to obtain the
+#   installation script and save it into the installation directory.
+Invoke-WebRequest `
+    -Uri "https://dot.net/v1/dotnet-install.ps1" `
+    -OutFile "$InstallDir/dotnet-install.ps1"
+
+Write-Host "Installing the CLI requested version ($CliVersion) ..."
+
+# Install the SDK of the version specified in $CliVersion into the
+#   specified location ($InstallDir).
+& $InstallDir/dotnet-install.ps1 -Version $CliVersion `
+    -InstallDir $InstallDir
+
+Write-Host "Downloading and installation of the SDK is complete."
+
+# $LocalDotnet holds the path to dotnet.exe for future use by the
+#   script.
+$LocalDotnet = "$InstallDir/dotnet"
+$env:DOTNET_ROOT = $LocalDotnet
+$ToolsDir = "$InstallDir/dotnet/tools"
+New-Item -Type "directory" -Path $ToolsDir
+cd $ToolsDir
+Invoke-WebRequest -Uri "https://aka.ms/dotnet-dump/win-x64" -OutFile dotnet-dump.exe
